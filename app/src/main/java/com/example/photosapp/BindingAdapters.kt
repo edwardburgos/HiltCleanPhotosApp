fcc5438df688961f2dc837ccd87f0ca272930a83
Edward Burgos <80774061+edwardburgos@users.noreply.github.com>
2022-02-12 12:59:58 -0500

    binding adapters observe Observable<ApiStatus>

--- app/src/main/java/com/example/photosapp/BindingAdapters.kt
+++ app/src/main/java/com/example/photosapp/BindingAdapters.kt
@@ -8,12 +8,14 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.request.RequestOptions
-import com.example.photosapp.recyclerview.adapters.PhotosSnapAdapter
 import com.bumptech.glide.load.model.LazyHeaders
 import com.bumptech.glide.load.model.GlideUrl
 import com.example.domain.ApiStatus
 import com.example.domain.Photo
 import com.example.photosapp.recyclerview.adapters.PhotosAdapter
+import com.example.photosapp.recyclerview.adapters.PhotosSnapAdapter
+import io.reactivex.Observable
+import io.reactivex.disposables.Disposable
 
 @BindingAdapter("listDataInSnap")
 fun bindRecyclerViewWithSnap(recyclerView: RecyclerView, data: List<Photo>?) {
@@ -31,13 +33,13 @@ fun bindRecyclerView(recyclerView: RecyclerView, data: List<Photo>?) {
 @BindingAdapter("imageUrl") // To use this * binding annotations *, 'kotlin-kapt' plugin is needed
 fun bindImage(imgView: ImageView, url: String?) {
     url?.let {
-        val url = GlideUrl(
+        val urlForGlide = GlideUrl(
             it, LazyHeaders.Builder()
                 .addHeader("User-Agent", WebSettings.getDefaultUserAgent(imgView.context))
                 .build()
         )
         Glide.with(imgView.context)
-            .load(url)
+            .load(urlForGlide)
             .apply(
                 RequestOptions()
                 .placeholder(R.drawable.loading_animation)
@@ -48,24 +50,36 @@ fun bindImage(imgView: ImageView, url: String?) {
 }
 
 @BindingAdapter("ApiStatus")
-fun bindStatus(statusImageView: ImageView, status: ApiStatus?) {
-    when (status) {
-        ApiStatus.LOADING -> {
-            statusImageView.visibility = View.VISIBLE
-            statusImageView.setImageResource(R.drawable.loading_animation)
-            statusImageView.getLayoutParams().height = 600;
-            statusImageView.getLayoutParams().width = 600;
-            statusImageView.requestLayout();
+fun bindStatusObservables(statusImageView: ImageView, status: Observable<ApiStatus>) {
+    status.subscribe(object : io.reactivex.Observer<ApiStatus> {
+
+        override fun onSubscribe(d: Disposable) { }
+
+        override fun onError(e: Throwable) { }
+
+        override fun onComplete() { }
+
+        override fun onNext(t: ApiStatus) {
+            when (t) {
+                ApiStatus.LOADING -> {
+                    statusImageView.visibility = View.VISIBLE
+                    statusImageView.setImageResource(R.drawable.loading_animation)
+                    statusImageView.layoutParams.height = 600
+                    statusImageView.layoutParams.width = 600
+                    statusImageView.requestLayout()
+                }
+                ApiStatus.ERROR -> {
+                    statusImageView.visibility = View.VISIBLE
+                    statusImageView.setImageResource(R.drawable.ic_bx_wifi_off)
+                    statusImageView.layoutParams.height = 200
+                    statusImageView.layoutParams.width = 200
+                    statusImageView.requestLayout()
+                }
+                else -> {
+                    statusImageView.visibility = View.GONE
+                }
+            }
         }
-        ApiStatus.ERROR -> {
-            statusImageView.visibility = View.VISIBLE
-            statusImageView.setImageResource(R.drawable.ic_bx_wifi_off)
-            statusImageView.getLayoutParams().height = 200;
-            statusImageView.getLayoutParams().width = 200;
-            statusImageView.requestLayout();
-        }
-        else -> {
-            statusImageView.visibility = View.GONE
-        }
-    }
+    })
+
 }
