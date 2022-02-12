cde14c22375c9dbbe8c0386eda6f1b7bd75d28d9
Edward Burgos <80774061+edwardburgos@users.noreply.github.com>
2022-02-12 12:48:50 -0500

    Parcelize import updated

--- data/src/main/java/com/example/data/network/model/PhotoApi.kt
+++ data/src/main/java/com/example/data/network/model/PhotoApi.kt
@@ -1,7 +1,7 @@
 package com.example.data.network.model
 
 import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class PhotoApi (
